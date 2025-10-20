fefa116086ba33daa3232afc44930b93d34328eb
Raj <bhosleraj252@gmail.com>
2025-10-20 23:11:32 +0530

    updated pom.xml

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -1,15 +1,13 @@
 package in.ashokit;
 
-/**
- * Hello world!
- */
+
 public class App {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
          int a = 10;
-=======
+
         String S = "Raj";
->>>>>>> 99227311fdac5588dbd15c12d8499dfe80fe27eb
+
         System.out.println("Hello World!");
     }
 }
