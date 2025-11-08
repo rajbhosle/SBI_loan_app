7e4bbe6704093a5017b8fdc4deaff01863c1d6aa
Raj <bhosleraj252@gmail.com>
2025-11-08 13:32:55 +0530

    initial commit

---
+++ src/main/java/in/ashokit/App.java
@@ -0,0 +1,16 @@
+package in.ashokit;
+
+public class App {
+    public static void main(String[] args) {
+
+          int i = 10;
+
+        String s = "Hello";
+
+        String s2 = "ball";
+
+
+
+        System.out.println("Hello World!");
+    }
+}
